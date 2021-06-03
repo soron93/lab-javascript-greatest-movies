682d2ddddc85d138ffcbb80152c015994bd1d15d
Shade <shade@Katana.localdomain>
2021-06-03 16:08:35 -0500

    worked on 5 & 6 but could not get working with function only when function is commented out will it run

--- src/movies.js
+++ src/movies.js
@@ -1,7 +1,14 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(movies) {
+  let mappedDirectors = movies.map((singleMovies)=>{
+    return singleMovies.director
+  })
+  
+  return mappedDirectors
+}
+
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 function howManyMovies() {}
@@ -15,8 +22,30 @@ function dramaMoviesScore() {}
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear() {}
 
+movies.sort(function (x, y){
+  return x.year - y.year;
+});
+
+console.log(movies)
+
+
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically() {}
+
+movies.sort(function (x, y) {
+  let a = x.title.toUpperCase(),
+      b = y.title.toUpperCase();
+return a == b ? 0 : a > b ? 1 : -1;
+})
+
+console.log(movies)
+
+
+
+
+
+
+
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
